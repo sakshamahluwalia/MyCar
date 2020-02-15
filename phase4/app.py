19815f16fab164481e615fdc51d482c49b8868c5
Saksham Ahluwalia <sakshamahluwalia5@gmail.com>
2020-02-15 15:39:55 -0500

    first successful test using flask

--- phase4/app.py
+++ phase4/app.py
@@ -1,13 +1,35 @@
+import obd
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
 global var
+global connection
+global rpm
+rpm = 0
 var = 0
+
+def initialize():
+
+    global connection
+    connection = obd.Async(portstr="/dev/rfcomm0", fast=False)
+
+    connection.watch(obd.commands.RPM, callback=update_rpm)
+    connection.start()
+
+def update_rpm(rev):
+    global rpm
+    rpm = rev.value.magnitude
+
+initialize()
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/rpm')
+def get_rpm():
+    global rpm
+    return {'rpm': rpm}
 
 @app.route('/val')
 def get_val():
@@ -19,3 +41,9 @@ def update():
     global var
     var += 1
     return {'res': 'updated'}
+
+@app.route('/end')
+def end():
+    global connection
+    connection.stop()
+    return "connection stopped"
